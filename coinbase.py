fe72090cc14c78e2d63a3f8e83f847a1a4e96835
GOKAY BURUC <gky.buruc@mail.ru>
2021-03-25 17:19:38 +0300

    Update coinbase.py

--- coinbase.py
+++ coinbase.py
@@ -6,6 +6,33 @@ from subprocess import run,Popen,call
 #import telegram-send
 #telegram_send.send(messages=["Wow that was easy!"])
 
+print("""
+
+SUPPORTED COINS :
+
+(CGLD), AAVE, ADA, ALGO, ANKR, ATOM,
+BAL, BAND, BAT, BCH, BNT, BSV, BTC,
+Celo, COMP, CRV, CVC,
+DAI, DASH, DNT,
+EOS, ETC, ETH,
+FIL,
+GNT, GRT,
+KNC,
+LINK, LOOM, LRC, LTC,
+MANA, MATIC, MKR, NMR,
+NU,
+OMG, OXT,
+REN, REP,
+SKL, SNX, STORJ, SUSHI,
+UMA, UNI, USDC,
+WBTC,
+XLM, XRP, XTZ,
+YFI,
+ZEC, ZRX
+
+""" )
+
+
 currName =str(input('CURRENCY   :\t')).strip()
 target = float(input('TARGET PRICE:\t'))
 print('interval must be bigger than 3 seconds')
@@ -21,22 +48,23 @@ while True:
     timestamp = time.ctime()
 
     print('='*50)
-    print(f'Time     : {timestamp}')
-    print(f'Currency : {currency}')
-    print(f'Price    : {price} $')
+    print(f'Time        : {timestamp}')
+    print(f'Currency    : {currency}')
+    print(f'Price       : {price} $')
 
     if float(price) > target:
-        print('High')
+        print('Sell')
         #telegram_send.send(messages=["You Can Sell Now!"])
-        command = ['telegram-send',f'You Can Sell Your {currName} Right Now!']
+        command = ['telegram-send',f'Target price reached : {target} . You Can Sell Your {currName} Right Now!']
         run(command)
+        break
 
     else:
         diff = round(target - float(price) , 2)
         diffPercent = round(100*(1 - (float(price) / target)) , 2)
-        print('Low')
-        print(f'Diff: -{diff}')
-        print(f'Diff Percent: - % {diffPercent}')
+        print('Status      : Hold')
+        print(f'Diff        : -{diff}')
+        print(f'Diff Percent: -%{diffPercent}')
         #telegram_send.send(messages=[f"Hold Your {currName}!"])
         #command = ['telegram-send',f'Hold your {currName}']
         #run(command)
